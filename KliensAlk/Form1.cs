0bad454e352893aaccdcbd5b9aa976f638a04b6a
mmikite <marton.mikite@stud.uni-corvinus.hu>
2024-04-15 16:55:36 +0200

    api kapcsolat működik

--- KliensAlk/Form1.cs
+++ KliensAlk/Form1.cs
@@ -20,7 +20,12 @@ namespace KliensAlk
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-
+			string url = "http://20.234.113.211:8107";
+			string key = "1-79771cd1-cb22-4710-a786-b360d8a92c2f";
+			var p = new Api(url, key);
+			var s = p.CategoriesFindAll();
+			string kiirnam = s.Content.Count.ToString();
+			textBox1.Text = kiirnam;
 		}
 	}
 }
