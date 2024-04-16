2257f95321db755fb36d9c8bf31485d3f39acf71
Márton Mikite <marton.mikite@stud.uni-corvinus.hu>
2024-04-16 14:38:18 +0200

    probalkozas termekszuressel, nagy szenvedesek

--- KliensAlk/Form1.cs
+++ KliensAlk/Form1.cs
@@ -1,4 +1,7 @@
-﻿using Hotcakes.CommerceDTO.v1.Client;
+﻿using Hotcakes.Commerce.Marketing.PromotionQualifications;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Catalog;
+using Hotcakes.CommerceDTO.v1.Client;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,19 +16,71 @@ namespace KliensAlk
 {
 	public partial class Form1 : Form
 	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-
-		private void Form1_Load(object sender, EventArgs e)
+		ApiResponse<List<ProductDTO>> termekadatok = new ApiResponse<List<ProductDTO>>();	
+		private static Api ApiKapcs()
 		{
 			string url = "http://20.234.113.211:8107";
 			string key = "1-79771cd1-cb22-4710-a786-b360d8a92c2f";
 			var p = new Api(url, key);
-			var s = p.CategoriesFindAll();
-			string kiirnam = s.Content.Count.ToString();
-			textBox1.Text = kiirnam;
+			return p;
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+			termekadatok = TermekAdatokKi();
+
+		}
+
+		private static ApiResponse<List<ProductDTO>> TermekAdatokKi()
+		{
+			Api p = ApiKapcs();
+			ApiResponse<List<ProductDTO>> termekadatok = p.ProductsFindAll();
+			return termekadatok;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			TermekNevSzures();
+
+		}
+
+		private void TermekNevSzures()
+		{
+			termekadatok = TermekAdatokKi();
+			var trmk = from x in termekadatok.Content.ToList()
+					   where x.ProductName.Contains(textBox1.Text)
+					   select x;
+			listBox1.DataSource = trmk.ToList();
+			listBox1.DisplayMember = "ProductName";
+			//listBox1.ValueMember = "";
+		}
+
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			TermekNevSzures();
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			Api p = ApiKapcs();
+			int id = listBox1.SelectedIndex;
+			//var inventoryId = "4d9443f7-589e-4618-a17b-85f1996250f9";
+			termekadatok = TermekAdatokKi();
+			var bvin = termekadatok.Content[id].Bvin;
+
+			string ar = p.ProductsFind(bvin).Content.ListPrice.ToString();
+
+			// call the API to find the product inventory record
+			//ApiResponse<ProductInventoryDTO> keszlet = p.ProductInventoryFind(inventoryId);
+
+			textBox2.Text = ar;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
