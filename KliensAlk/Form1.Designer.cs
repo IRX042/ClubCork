4d59e6bbe29108737e43e398a160ce42bfa1e49c
Márton Mikite <marton.mikite@stud.uni-corvinus.hu>
2024-04-20 18:24:02 +0200

    mukodik a vonalkodosdi

--- KliensAlk/Form1.Designer.cs
+++ KliensAlk/Form1.Designer.cs
@@ -42,12 +42,13 @@
 			this.buttonMentes = new System.Windows.Forms.Button();
 			this.buttonBeolvasas = new System.Windows.Forms.Button();
 			this.label4 = new System.Windows.Forms.Label();
+			this.buttonTorles = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// textBoxNev
 			// 
-			this.textBoxNev.Location = new System.Drawing.Point(129, 32);
+			this.textBoxNev.Location = new System.Drawing.Point(82, 14);
 			this.textBoxNev.Name = "textBoxNev";
 			this.textBoxNev.Size = new System.Drawing.Size(402, 20);
 			this.textBoxNev.TabIndex = 0;
@@ -57,9 +58,9 @@
 			// 
 			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.listBox1.FormattingEnabled = true;
-			this.listBox1.Location = new System.Drawing.Point(59, 84);
+			this.listBox1.Location = new System.Drawing.Point(12, 73);
 			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(472, 355);
+			this.listBox1.Size = new System.Drawing.Size(472, 342);
 			this.listBox1.TabIndex = 1;
 			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
 			// 
@@ -67,7 +68,7 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-			this.label1.Location = new System.Drawing.Point(589, 59);
+			this.label1.Location = new System.Drawing.Point(542, 41);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(98, 17);
 			this.label1.TabIndex = 2;
@@ -76,14 +77,14 @@
 			// textBoxRaktar
 			// 
 			this.textBoxRaktar.Enabled = false;
-			this.textBoxRaktar.Location = new System.Drawing.Point(693, 58);
+			this.textBoxRaktar.Location = new System.Drawing.Point(646, 40);
 			this.textBoxRaktar.Name = "textBoxRaktar";
 			this.textBoxRaktar.Size = new System.Drawing.Size(100, 20);
 			this.textBoxRaktar.TabIndex = 3;
 			// 
 			// buttonPlusz
 			// 
-			this.buttonPlusz.Location = new System.Drawing.Point(810, 44);
+			this.buttonPlusz.Location = new System.Drawing.Point(763, 26);
 			this.buttonPlusz.Name = "buttonPlusz";
 			this.buttonPlusz.Size = new System.Drawing.Size(34, 23);
 			this.buttonPlusz.TabIndex = 4;
@@ -93,7 +94,7 @@
 			// 
 			// buttonMinusz
 			// 
-			this.buttonMinusz.Location = new System.Drawing.Point(810, 73);
+			this.buttonMinusz.Location = new System.Drawing.Point(763, 55);
 			this.buttonMinusz.Name = "buttonMinusz";
 			this.buttonMinusz.Size = new System.Drawing.Size(34, 23);
 			this.buttonMinusz.TabIndex = 5;
@@ -104,7 +105,7 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(56, 35);
+			this.label2.Location = new System.Drawing.Point(9, 17);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(67, 13);
 			this.label2.TabIndex = 6;
@@ -112,7 +113,7 @@
 			// 
 			// buttonKeszites
 			// 
-			this.buttonKeszites.Location = new System.Drawing.Point(694, 189);
+			this.buttonKeszites.Location = new System.Drawing.Point(545, 193);
 			this.buttonKeszites.Name = "buttonKeszites";
 			this.buttonKeszites.Size = new System.Drawing.Size(80, 23);
 			this.buttonKeszites.TabIndex = 7;
@@ -122,16 +123,16 @@
 			// 
 			// textBoxSKU
 			// 
-			this.textBoxSKU.Location = new System.Drawing.Point(129, 58);
+			this.textBoxSKU.Location = new System.Drawing.Point(82, 40);
 			this.textBoxSKU.Name = "textBoxSKU";
 			this.textBoxSKU.Size = new System.Drawing.Size(402, 20);
 			this.textBoxSKU.TabIndex = 8;
-			this.textBoxSKU.Text = "123456";
+			this.textBoxSKU.TextChanged += new System.EventHandler(this.textBoxSKU_TextChanged);
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(56, 61);
+			this.label3.Location = new System.Drawing.Point(9, 43);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(71, 13);
 			this.label3.TabIndex = 9;
@@ -139,15 +140,15 @@
 			// 
 			// pictureBox1
 			// 
-			this.pictureBox1.Location = new System.Drawing.Point(592, 218);
+			this.pictureBox1.Location = new System.Drawing.Point(631, 182);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(431, 162);
+			this.pictureBox1.Size = new System.Drawing.Size(166, 112);
 			this.pictureBox1.TabIndex = 10;
 			this.pictureBox1.TabStop = false;
 			// 
 			// buttonMentes
 			// 
-			this.buttonMentes.Location = new System.Drawing.Point(780, 189);
+			this.buttonMentes.Location = new System.Drawing.Point(545, 251);
 			this.buttonMentes.Name = "buttonMentes";
 			this.buttonMentes.Size = new System.Drawing.Size(80, 23);
 			this.buttonMentes.TabIndex = 11;
@@ -157,7 +158,7 @@
 			// 
 			// buttonBeolvasas
 			// 
-			this.buttonBeolvasas.Location = new System.Drawing.Point(866, 189);
+			this.buttonBeolvasas.Location = new System.Drawing.Point(545, 222);
 			this.buttonBeolvasas.Name = "buttonBeolvasas";
 			this.buttonBeolvasas.Size = new System.Drawing.Size(80, 23);
 			this.buttonBeolvasas.TabIndex = 12;
@@ -169,17 +170,28 @@
 			// 
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-			this.label4.Location = new System.Drawing.Point(589, 192);
+			this.label4.Location = new System.Drawing.Point(542, 173);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(71, 17);
 			this.label4.TabIndex = 13;
 			this.label4.Text = "Vonalkód:";
 			// 
+			// buttonTorles
+			// 
+			this.buttonTorles.Location = new System.Drawing.Point(545, 280);
+			this.buttonTorles.Name = "buttonTorles";
+			this.buttonTorles.Size = new System.Drawing.Size(80, 23);
+			this.buttonTorles.TabIndex = 14;
+			this.buttonTorles.Text = "Törlés";
+			this.buttonTorles.UseVisualStyleBackColor = true;
+			this.buttonTorles.Click += new System.EventHandler(this.buttonTorles_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1078, 450);
+			this.ClientSize = new System.Drawing.Size(819, 457);
+			this.Controls.Add(this.buttonTorles);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.buttonBeolvasas);
 			this.Controls.Add(this.buttonMentes);
@@ -195,7 +207,7 @@
 			this.Controls.Add(this.listBox1);
 			this.Controls.Add(this.textBoxNev);
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = " ";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
@@ -219,6 +231,7 @@
 		private System.Windows.Forms.Button buttonMentes;
 		private System.Windows.Forms.Button buttonBeolvasas;
 		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button buttonTorles;
 	}
 }
 
--- KliensAlk/Form1.cs
+++ KliensAlk/Form1.cs
@@ -2,7 +2,7 @@
 using Hotcakes.CommerceDTO.v1;
 using Hotcakes.CommerceDTO.v1.Catalog;
 using Hotcakes.CommerceDTO.v1.Client;
-using IronBarCode;
+using Hotcakes.Web.Barcodes;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,6 +13,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
 
 namespace KliensAlk
 {
@@ -43,10 +44,11 @@ namespace KliensAlk
 		{
 			public string ProductName { get; set; }
 			public string Bvin { get; set; }
+			public string ProductSKU { get; set; }	
 
 			public override string ToString()
 			{
-				return $"{ProductName}_{Bvin}";
+				return $"{ProductName}_{Bvin}_{ProductSKU}";
 			}
 		}
 
@@ -57,6 +59,7 @@ namespace KliensAlk
 					   select new TermekListaElem
 					   {
 						   ProductName = x.ProductName,
+						   ProductSKU = x.Sku,
 						   Bvin = x.Bvin,
 					   };
 			listBox1.DataSource = trmk.ToList();
@@ -67,6 +70,20 @@ namespace KliensAlk
 		private void textBox1_TextChanged(object sender, EventArgs e)
 		{
 			TermekNevSzures();
+		}
+
+		private void textBoxSKU_TextChanged(object sender, EventArgs e)
+		{
+			var trmk = from x in termekadatok.Content.ToList()
+					   where x.Sku == textBoxSKU.Text
+					   select new TermekListaElem
+					   {
+						   ProductName = x.ProductName,
+						   ProductSKU = x.Sku,
+						   Bvin = x.Bvin,
+					   };
+			listBox1.DataSource = trmk.ToList();
+			listBox1.DisplayMember = "ProductName";
 		}
 
 		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -144,17 +161,55 @@ namespace KliensAlk
 
 		private void buttonKeszites_Click(object sender, EventArgs e)
 		{
+			string sku = listBox1.SelectedItem.ToString().Split('_')[2];
+			BarcodeWriter writer = new BarcodeWriter() { Format = BarcodeFormat.CODE_128};
+			pictureBox1.Image = writer.Write(sku);
 
+			
 		}
 
 		private void buttonMentes_Click(object sender, EventArgs e)
 		{
+			SaveFileDialog sf = new SaveFileDialog();
+			sf.Filter = "JPEG Image|*.jpg|PNG Image|*.png|BMP Image|*.bmp|All files|*.*";
+			sf.Title = "Vonalkód mentése képként";
+			sf.ShowDialog();
 
+			if (sf.FileName != "")
+			{
+				try
+				{
+					Image img = pictureBox1.Image;
+					img.Save(sf.FileName);
+
+					MessageBox.Show("Kép mentve!");
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Hiba a kép mentése közben: " + ex.Message);
+				}
+			}
 		}
 
 		private void buttonBeolvasas_Click(object sender, EventArgs e)
 		{
+			OpenFileDialog of = new OpenFileDialog();
+			of.Filter = "Image Files (*.jpg;*.jpeg;*.gif;*.bmp;*.png)|*.jpg;*.jpeg;*.gif;*.bmp;*.png";
 
+			if (of.ShowDialog() == DialogResult.OK)
+			{
+				pictureBox1.Image = Image.FromFile(of.FileName);
+				BarcodeReader reader = new BarcodeReader();
+				var sku = reader.Decode((Bitmap)pictureBox1.Image);
+				if (sku != null) { textBoxSKU.Text = sku.Text; }
+			}
+		}
+
+		private void buttonTorles_Click(object sender, EventArgs e)
+		{
+			pictureBox1.Image = null;
+			textBoxSKU.Text = null;
+			TermekNevSzures();
 		}
 	}
 }
